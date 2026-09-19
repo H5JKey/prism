65afadb35739837e05eda18377d7c0eafdbe9ddf
Nikolay Shirokov <shirkol2006@mail.ru>
2026-09-19 17:32:13 +0300

    refactor(tests): split project tests into separate files

---
+++ backend/tests/test_schemas/test_project/conftest.py
@@ -0,0 +1,146 @@
+import pytest
+
+
+@pytest.fixture
+def project_base_data() -> dict:
+    return {
+        "name": "project_name",
+        "description": "description",
+        "source_file_id": 1,
+    }
+
+
+@pytest.fixture
+def project_create_data(project_base_data: dict) -> dict:
+    return {
+        **project_base_data,
+        "visibility": "public",
+    }
+
+
+@pytest.fixture
+def project_with_render_create_data(
+    render_create_payload_data: dict,
+    project_create_data: dict,
+) -> dict:
+    return {
+        "render": render_create_payload_data,
+        "project": project_create_data,
+    }
+
+
+@pytest.fixture
+def project_partial_update_data() -> dict:
+    return {
+        "name": "project_name",
+        "description": "description",
+        "visibility": "public",
+    }
+
+
+@pytest.fixture
+def project_response_data(project_base_data: dict) -> dict:
+    return {
+        **project_base_data,
+        "visibility": "public",
+        "status": "rendering",
+        "user_id": 1,
+        "render_id": 1,
+        "id": 1,
+    }
+
+
+@pytest.fixture
+def project_full_response_data(project_response_data: dict) -> dict:
+    return {
+        **project_response_data,
+        "url": "http://example.com",
+    }
+
+
+@pytest.fixture
+def project_with_render_file_full_response_data(
+    project_full_response_data: dict,
+    render_with_file_full_response_data: dict,
+) -> dict:
+    return {
+        **project_full_response_data,
+        "render": render_with_file_full_response_data,
+    }
+
+
+@pytest.fixture
+def project_with_render_response_data(
+    project_response_data: dict,
+    render_response_data: dict,
+) -> dict:
+    return {
+        **project_response_data,
+        "render": render_response_data,
+    }
+
+
+@pytest.fixture
+def project_with_render_file_response_data(
+    project_response_data: dict,
+    render_with_file_response_data: dict,
+) -> dict:
+    return {
+        **project_response_data,
+        "render": render_with_file_response_data,
+    }
+
+
+@pytest.fixture
+def project_response_list_data() -> dict:
+    return {
+        "project_list": [
+            {
+                "name": "project_name1",
+                "description": "description1",
+                "source_file_id": 1,
+                "visibility": "public",
+                "status": "rendering",
+                "user_id": 1,
+                "render_id": 1,
+                "id": 1,
+            },
+            {
+                "name": "project_name2",
+                "description": "description2",
+                "source_file_id": 2,
+                "visibility": "private",
+                "status": "rendering",
+                "user_id": 2,
+                "render_id": 2,
+                "id": 2,
+            },
+        ],
+        "size": 1,
+        "page": 1,
+    }
+
+
+@pytest.fixture
+def project_with_render_file_response_list_data() -> dict:
+    return {
+        "visibility": "public",
+        "status": "rendering",
+        "user_id": 1,
+        "render_id": 1,
+        "id": 1,
+        "render": {
+            "width": 1000,
+            "height": 1000,
+            "samples": 150,
+            "denoiser": True,
+            "gpu": False,
+            "id": 1,
+            "file_id": 1,
+            "file": {
+                "name": "file_name",
+                "size": 1000,
+                "id": 1,
+            },
+        },
+    }
