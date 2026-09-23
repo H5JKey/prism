828f50cc023e71a5815fc21ae7842f0a4d787a88
Nikolay Shirokov <shirkol2006@mail.ru>
2026-09-23 18:56:02 +0300

    test(file repository): test file repository methods

---
+++ backend/tests/test_infrastructure/test_repositories/test_file.py
@@ -0,0 +1,59 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from infrastructure.database.repositories import FileRepository
+from schemas.file import FileCreate
+from tests.test_infrastructure.test_database.test_models.factories import create_file
+
+
+class TestFileRepository:
+    async def test_file_repository_get_by_id(
+        self,
+        session: AsyncSession,
+        file_repository: FileRepository,
+    ) -> None:
+        file = await create_file(session)
+        session.expunge(file)
+        selected_file = await file_repository.get_by_id(file.id)
+        assert selected_file.id == file.id
+        assert selected_file.name == file.name
+        assert selected_file.size == file.size
+        assert selected_file.bucket == file.bucket
+        assert selected_file.key == file.key
+
+    async def test_file_repository_get_by_id_not_exists_file(
+        self,
+        file_repository: FileRepository,
+    ) -> None:
+        selected_file = await file_repository.get_by_id(-1)
+        assert selected_file is None
+
+    async def test_file_repository_create_file(
+        self,
+        session: AsyncSession,
+        file_repository: FileRepository,
+    ) -> None:
+        create_file_data = FileCreate(
+            name="file_name",
+            size=1024,
+            bucket="bucket",
+            key="key",
+        )
+        created_file = await file_repository.create_file(create_file_data)
+        session.expunge(created_file)
+        selected_file = await file_repository.get_by_id(created_file.id)
+        assert selected_file.id is not None
+        assert selected_file.name == create_file_data.name
+        assert selected_file.size == create_file_data.size
+        assert selected_file.bucket == create_file_data.bucket
+        assert selected_file.key == create_file_data.key
+
+    async def test_file_repository_delete_by_id(
+        self,
+        session: AsyncSession,
+        file_repository: FileRepository,
+    ) -> None:
+        file = await create_file(session)
+        await file_repository.delete_by_id(file.id)
+        session.expunge(file)
+        deleted_file = await file_repository.get_by_id(file.id)
+        assert deleted_file is None
